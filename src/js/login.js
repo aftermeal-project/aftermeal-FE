0d01c478942dd3283e5adfcd260a450f892ff1ab
jongjinnn <jongjin.me@gmail.com>
2024-08-04 18:50:44 +0900

    fix: 파일 구조 수정

--- src/js/login.js
+++ src/js/login.js
@@ -1,31 +1,35 @@
-document.getElementById('loginForm')?.addEventListener('submit',  (event)=> login(event));
+import "../styles/login.css";
+
+document
+  .getElementById("loginForm")
+  ?.addEventListener("submit", (event) => login(event));
 
 async function login(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
+  event.preventDefault(); // Prevent the form from submitting the traditional way
 
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
 
-    // Simulate an API call for login
-    try {
-        const response = await fetch('https://api.aftermeal.online/v1/auth/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email, password })
-        });
-        if (response.ok) {
-            const { data } = await response.json();
-            // Store received token for future requests
-            localStorage.setItem('accessToken', data.accessToken);
-            alert('Login successful.');
-            window.location.href = 'index.html'; // Redirect to main page
-        } else {
-            alert('Login failed. Please check your credentials.');
-        }
-    } catch (error) {
-        console.error('Error:', error.response);
-        alert('An error occurred during login. Please try again.');
+  // Simulate an API call for login
+  try {
+    const response = await fetch("https://api.aftermeal.online/v1/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    if (response.ok) {
+      const { data } = await response.json();
+      // Store received token for future requests
+      localStorage.setItem("accessToken", data.accessToken);
+      alert("Login successful.");
+      window.location.href = "index.html"; // Redirect to main page
+    } else {
+      alert("Login failed. Please check your credentials.");
     }
+  } catch (error) {
+    console.error("Error:", error.response);
+    alert("An error occurred during login. Please try again.");
+  }
 }
