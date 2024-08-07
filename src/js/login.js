c7ff9e1963b8df8a9dd418670308ac994469bbd9
jongjinnn <jongjin.me@gmail.com>
2024-08-07 18:54:22 +0900

    chore: login api 분리

--- src/js/login.js
+++ src/js/login.js
@@ -1,6 +1,6 @@
-import { BASE_URL } from "../constants/index.js";
+import { loginApi } from "../libs/api/auth.js";
 import "../styles/login.css";
-import TokenService from "../utils/tokenService.js";
+import tokenService from "../utils/tokenService.js";
 
 document
   .getElementById("loginForm")
@@ -8,28 +8,19 @@ document
 
 async function login(event) {
   event.preventDefault();
-
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   try {
-    const response = await fetch(BASE_URL + "/v1/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
-    if (response.ok) {
-      const { data } = await response.json();
-      TokenService.setUser(data);
-      alert("Login successful.");
-      window.location.href = "index.html";
-    } else {
-      alert("Login failed. Please check your credentials.");
-    }
+    const response = await loginApi(email, password);
+    tokenService.setUser(response?.data);
+    alert("Login successful.");
+    window.location.href = "index.html";
   } catch (error) {
-    console.error("Error:", error.response);
-    alert("An error occurred during login. Please try again.");
+    if (error?.response?.status === 404) {
+      alert("Login failed. Please check your credentials.");
+    } else {
+      alert("An error occurred during login. Please try again.");
+    }
   }
 }
