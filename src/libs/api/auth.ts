b22e01fc6e6b1edc05935f1aa291a4e864e5cfbd
jongjinnn <jongjin.me@gmail.com>
2024-08-14 22:15:03 +0900

    add: LoginAPI

---
+++ src/libs/api/auth.ts
@@ -0,0 +1,12 @@
+import { LoginRequest, LoginResponse } from '../../types/auth';
+import { instance } from '../instance';
+
+export const LoginAPI = async (loginRequest: LoginRequest) => {
+  const response = await instance<LoginResponse>({
+    method: 'POST',
+    url: '/v1/auth/login',
+    data: loginRequest,
+  });
+
+  return response;
+};
