f52994d087ae56ca26a5dd859dcec52d342f4d3c
jongjinnn <jongjin.me@gmail.com>
2024-08-14 21:44:17 +0900

    feat: 특정 domain일 때 header 숨기기

--- src/domains/@global/layout/index.tsx
+++ src/domains/@global/layout/index.tsx
@@ -1,10 +1,18 @@
+import { useLocation } from 'react-router-dom';
 import Routing from '../../../routes/routing';
 import Header from '../header';
+import Path from '../../../routes/path';
 
 export default function Layout() {
+  const location = useLocation();
+
+  const isNotAuthDomain =
+    location.pathname !== Path.LoginPage &&
+    location.pathname !== Path.SignupPage;
+
   return (
     <>
-      <Header />
+      {isNotAuthDomain && <Header />}
       <Routing />
     </>
   );
