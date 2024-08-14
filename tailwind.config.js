6fa41558448327afd42ebcc35b17ef499725e999
jongjinnn <jongjin.me@gmail.com>
2024-08-14 21:11:25 +0900

    add: background gradients

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'header-gradient': 'linear-gradient(45deg, #007bff, #5563DE)',
+        'auth-gradient': 'linear-gradient(135deg, #74ABE2 10%, #5563DE 100%)',
+      },
+    },
   },
   plugins: [],
 };
