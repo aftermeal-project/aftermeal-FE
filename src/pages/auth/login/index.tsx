10566af8cf6e0dcfc1a0f602307539dff97eb72e
jongjinnn <jongjin.me@gmail.com>
2024-08-19 16:48:11 +0900

    add: LoginPage 컴포넌트 화

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -3,16 +3,20 @@ import { LoginRequest } from '../../../types/auth';
 import { AxiosError } from 'axios';
 import { LoginAPI } from '../../../libs/api/auth';
 import Token from '../../../libs/utils/token';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import AuthButton from '../../../components/auth/button';
+import AuthFormContainer from '../../../components/auth/container';
+import AuthInput from '../../../components/auth/input';
 
 export default function LoginPage() {
+  const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
     setError,
     formState: { errors },
   } = useForm<LoginRequest>();
-  const navigate = useNavigate();
 
   const onValid: SubmitHandler<LoginRequest> = async data => {
     try {
@@ -40,67 +44,39 @@ export default function LoginPage() {
     }
   };
 
-  const onInvalid = () => {};
-
   return (
-    <main className="flex items-center justify-center h-screen bg-gradient-to-br from-blue-300 to-indigo-600">
-      <section className="p-8 text-center bg-white shadow-lg w-96 rounded-xl">
-        <header>
-          <h1 className="mb-5 text-2xl font-bold text-gray-800">에프터밀</h1>
-        </header>
-        <form id="loginForm" onSubmit={handleSubmit(onValid, onInvalid)}>
-          <div className="mb-5">
-            <label htmlFor="email" className="sr-only">
-              이메일
-            </label>
-            <input
-              id="email"
-              type="email"
-              placeholder="이메일"
-              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              {...register('email', { required: '이메일은 필수 입력입니다.' })}
-            />
-          </div>
-          <div className={`${errors.email || errors.password ? '' : 'mb-7'}`}>
-            <label htmlFor="password" className="sr-only">
-              비밀번호
-            </label>
-            <input
-              id="password"
-              type="password"
-              placeholder="비밀번호"
-              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              {...register('password', {
-                required: '비밀번호는 필수 입력입니다.',
-              })}
-            />
-          </div>
-          {errors.email && (
-            <p className="my-5 text-left text-[13px] leading-4 tracking-[-.75px] text-[#ff0101]">
-              {errors.email.message}
-            </p>
-          )}
-          {!errors.email && errors.password && (
-            <p className="my-5 text-left text-[13px] leading-4 tracking-[-.75px] text-[#ff0101]">
-              {errors.password.message}
-            </p>
-          )}
-          <button
-            type="submit"
-            className="w-full p-3 font-medium text-white transition duration-300 bg-indigo-600 rounded -md hover:bg-indigo-500"
-          >
-            로그인
-          </button>
-          {/* <div className="mt-5">
-            <Link
-              to="/signup"
-              className="text-sm text-[#0b57d0] transition duration-300 hover:underline"
-            >
-              계정 만들기
-            </Link>
-          </div> */}
-        </form>
-      </section>
-    </main>
+    <AuthFormContainer title="애프터밀">
+      <form id="loginForm" onSubmit={handleSubmit(onValid)}>
+        <AuthInput<LoginRequest>
+          label="이메일"
+          name="email"
+          type="email"
+          placeholder="이메일"
+          register={register}
+          validationRules={{ required: '이메일은 필수 입력입니다.' }}
+          margin="mb-4"
+        />
+        <AuthInput<LoginRequest>
+          label="비밀번호"
+          name="password"
+          type="password"
+          placeholder="비밀번호"
+          register={register}
+          validationRules={{ required: '비밀번호는 필수 입력입니다.' }}
+          margin={`${errors.email || errors.password ? '' : 'mb-7'}`}
+        />
+        {errors.email && (
+          <p className="my-5 text-left text-[13px] leading-4 tracking-[-.75px] text-[#ff0101]">
+            {errors.email.message}
+          </p>
+        )}
+        {!errors.email && errors.password && (
+          <p className="my-5 px-2 text-left text-[13px] leading-4 tracking-[-.75px] text-[#ff0101]">
+            {errors.password.message}
+          </p>
+        )}
+        <AuthButton text="로그인" type="submit" />
+      </form>
+    </AuthFormContainer>
   );
 }
