28c126ec9998ca70177607870e5dec70b3ed16a2
jongjinnn <jongjin.me@gmail.com>
2024-08-19 15:58:56 +0900

    feat: api error handler 구현

--- src/libs/instance.ts
+++ src/libs/instance.ts
@@ -1,6 +1,15 @@
 import axios from 'axios';
 import { BASE_URL } from '../constants/url';
+import { errorHandler } from './utils/error.handler';
 
 export const instance = axios.create({
   baseURL: BASE_URL,
 });
+
+instance.interceptors.response.use(
+  response => response,
+  error => {
+    errorHandler(error);
+    return Promise.reject(error);
+  },
+);
