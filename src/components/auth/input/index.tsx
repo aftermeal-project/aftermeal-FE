983877218694b0f3e62f9a6f03d6aee7f42c0acd
jongjinnn <jongjin.me@gmail.com>
2024-08-19 16:47:54 +0900

    add: AuthInput

---
+++ src/components/auth/input/index.tsx
@@ -0,0 +1,37 @@
+import { InputHTMLAttributes } from 'react';
+import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
+
+interface AuthInputProps<T extends FieldValues>
+  extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  name: Path<T>;
+  register: UseFormRegister<T>;
+  validationRules?: object;
+  margin?: string;
+  error?: string;
+}
+
+export default function AuthInput<T extends FieldValues>({
+  label,
+  name,
+  register,
+  validationRules,
+  margin,
+  error,
+  ...rest
+}: AuthInputProps<T>) {
+  return (
+    <div className={`text-left ${margin}`}>
+      <label htmlFor={rest.id} className="sr-only">
+        {label}
+      </label>
+      <input
+        className={`w-full rounded-md border p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500 ${
+          error ? 'border-red-500' : 'border-gray-300'
+        }`}
+        {...register(name, validationRules)}
+        {...rest}
+      />
+    </div>
+  );
+}
