db3f2c1b15fb8ecbf4ad28ff19a7228e6032e2a3
jongjinnn <jongjin.me@gmail.com>
2024-08-20 01:48:40 +0900

    add: Refresh API

--- src/libs/api/auth.ts
+++ src/libs/api/auth.ts
@@ -1,11 +1,24 @@
 import { LoginRequest, LoginResponse } from '../../types/auth';
 import { instance } from '../instance';
+import token from '../utils/token';
 
 export const LoginAPI = async (loginRequest: LoginRequest) => {
   const response = await instance<LoginResponse>({
     method: 'POST',
     url: '/v1/auth/login',
     data: loginRequest,
+  });
+
+  return response.data;
+};
+
+export const RefreshAPI = async () => {
+  const response = await instance<LoginResponse>({
+    method: 'POST',
+    url: '/v1/auth/refresh',
+    data: {
+      refreshToken: token.getLocalRefreshToken(),
+    },
   });
 
   return response.data;
