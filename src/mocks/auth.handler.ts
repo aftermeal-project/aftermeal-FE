dc33242249602b1681c39a195557a2682082b844
jongjinnn <jongjin.me@gmail.com>
2024-08-19 16:00:23 +0900

    chore: login mocking api 개선

--- src/mocks/auth.handler.ts
+++ src/mocks/auth.handler.ts
@@ -1,18 +1,18 @@
 import { http, HttpResponse } from 'msw';
 import { LoginRequest } from '../types/auth';
+import { BASE_URL } from '../constants/url';
 
 export const authHandlers = [
-  http.post<LoginRequest>('/api/login', async ({ request }) => {
+  http.post<LoginRequest>(BASE_URL + '/v1/auth/login', async ({ request }) => {
     const { email, password } = (await request.json()) as LoginRequest;
-
     if (email !== 'test@example.com' || password !== '12345678') {
       return HttpResponse.json(null, {
         status: 404,
       });
     } else {
       return HttpResponse.json({
-        accessToken: 'dse21rf-24212rf',
-        refreshToken: 'ddw222r-ccswff2',
+        accessToken: String(crypto.randomUUID()),
+        refreshToken: String(crypto.randomUUID()),
       });
     }
   }),
