98078f07907e0101f71905d1f497725eddfa3b36
jongjinnn <jongjin.me@gmail.com>
2024-08-20 09:47:56 +0900

    add: HomePage UI 구현

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,36 @@
+import { useEffect, useState } from 'react';
+import { GetActivitiesAPI } from '../../libs/api/activities';
+import { Activity } from '../../types/activities';
+import ActivityCard from '../../components/home/card';
+
 export default function HomePage() {
-  return <section></section>;
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    const getActivities = async () => {
+      try {
+        const response = await GetActivitiesAPI();
+        setActivities(response.data);
+      } catch {
+        throw new Error('Failed to get activities data');
+      }
+    };
+
+    getActivities();
+  }, []);
+  return (
+    <main className="w-full px-4 py-8">
+      <div className="grid max-w-screen-xl grid-cols-1 gap-6 mx-auto sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {activities.map(activity => (
+          <ActivityCard
+            key={activity.id}
+            id={activity.id}
+            name={activity.name}
+            currentParticipants={activity.currentParticipants}
+            maxParticipants={activity.maxParticipants}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
