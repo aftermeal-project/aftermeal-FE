f9e01f6473e6a0799524d4ac5bf4dad652be80ae
jongjinnn <jongjin.me@gmail.com>
2024-08-22 21:50:02 +0900

    add: getActivitiesErrorMessages

---
+++ src/constants/messages/getActivitiesErrorMessages.ts
@@ -0,0 +1,8 @@
+export const getActivitiesErrorMessages = {
+  NETWORK_ERROR:
+    '이런! 네트워크에 문제가 발생했습니다. 네트워크를 확인해주세요.',
+  SERVER_ERROR:
+    '죄송합니다. 현재 서버에 문제가 발생했습니다. 잠시 후 다시 시도해 주세요.',
+  UNKNOWN_ERROR: '알 수 없는 오류입니다. 잠시 후 다시 접속해주세요.',
+  NORMAL_ERROR: '오류가 발생했습니다. 잠시 후 다시 시도해 주세요.',
+};
