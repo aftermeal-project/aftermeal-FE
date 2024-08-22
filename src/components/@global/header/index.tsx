6570679f68b79e8efa28bc3599ba094aaed3d26d
jongjinnn <jongjin.me@gmail.com>
2024-08-22 20:20:02 +0900

    add: 로그인 시에만 띄워줄 Header UI 추가

--- src/components/@global/header/index.tsx
+++ src/components/@global/header/index.tsx
@@ -1,7 +1,14 @@
 import { useNavigate } from 'react-router-dom';
+import token from '../../../libs/utils/token';
 
 export default function Header() {
   const navigate = useNavigate();
+
+  function onLogout() {
+    token.removeUser();
+    alert('로그아웃 되었습니다.');
+    navigate('/login');
+  }
 
   return (
     <header className="relative flex items-center w-full h-16 px-4 bg-header-gradient sm:h-20 lg:h-24">
@@ -19,14 +26,25 @@ export default function Header() {
         </div>
         <nav>
           <ul className="flex space-x-6 text-base sm:text-lg">
-            <li>
-              <a
-                href="/login"
-                className="transition duration-300 ease-in-out hover:text-yellow-400 active:text-yellow-500"
-              >
-                로그인
-              </a>
-            </li>
+            {token.getLocalAccessToken() ? (
+              <li>
+                <span
+                  onClick={onLogout}
+                  className="transition duration-300 ease-in-out cursor-pointer hover:text-yellow-400 active:text-yellow-500"
+                >
+                  로그아웃
+                </span>
+              </li>
+            ) : (
+              <li>
+                <a
+                  href="/login"
+                  className="transition duration-300 ease-in-out hover:text-yellow-400 active:text-yellow-500"
+                >
+                  로그인
+                </a>
+              </li>
+            )}
           </ul>
         </nav>
       </div>
