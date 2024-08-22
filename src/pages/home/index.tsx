110f30c32751f3731e95b93ba74b03a881089f21
jongjinnn <jongjin.me@gmail.com>
2024-08-22 21:51:26 +0900

    feat: getActivities error handling

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,27 +4,38 @@ import { Activity } from '../../types/activities';
 import ActivityCard from '../../components/home/card';
 import { ParticipationAPI } from '../../libs/api/participation';
 import { AxiosError } from 'axios';
+import GetActivitiesErrorScreen from '../../components/home/error';
+import { getActivitiesErrorMessages } from '../../constants/messages/getActivitiesErrorMessages';
 
 export default function HomePage() {
   const [activities, setActivities] = useState<Activity[]>([]);
-  const [error, setError] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
 
-  async function fetchActivities() {
-    try {
-      const response = await GetActivitiesAPI();
-      setActivities(response.data);
-    } catch {
-      setError(true);
+  function handleParticipationError(error: any) {
+    if (error instanceof AxiosError) {
+      const { response } = error;
+
+      if (response?.status === 400 || response?.status === 404) {
+        alert('신청할 수 없는 활동입니다.');
+      }
     }
   }
 
-  function handleParticipationError(error: unknown) {
-    if (error instanceof AxiosError) {
-      const code = error.response?.status;
+  function handleFetchError(error: any) {
+    const { response } = error;
 
-      if (code === 400 || code === 404) {
-        alert('신청할 수 없는 활동입니다.');
+    if (!response) {
+      setError(getActivitiesErrorMessages.NETWORK_ERROR);
+    }
+
+    if (error instanceof AxiosError) {
+      if (response.status === 500) {
+        setError(getActivitiesErrorMessages.SERVER_ERROR);
+      } else {
+        setError(getActivitiesErrorMessages.NORMAL_ERROR);
       }
+    } else {
+      setError(getActivitiesErrorMessages.UNKNOWN_ERROR);
     }
   }
 
@@ -37,9 +48,22 @@ export default function HomePage() {
     }
   }
 
+  async function fetchActivities() {
+    try {
+      const response = await GetActivitiesAPI();
+      setActivities(response.data);
+    } catch (error: unknown) {
+      handleFetchError(error);
+    }
+  }
+
   useEffect(() => {
     fetchActivities();
   }, []);
+
+  if (error) {
+    return <GetActivitiesErrorScreen message={error} />;
+  }
 
   return (
     <main className="w-full px-4 py-8">
