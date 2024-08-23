f5a1508e0b6f61f459a999eea848c976700441f7
jongjinnn <jongjin.me@gmail.com>
2024-08-23 16:02:23 +0900

    refactor: notfound page import & export 방식 변경

--- src/pages/not-found/NotFoundPage.tsx
+++ src/pages/not-found/NotFoundPage.tsx
@@ -1,4 +1,4 @@
-import ErrorScreen from '../../components/@global/error';
+import { ErrorScreen } from '../../components/@global';
 
 export default function NotFoundPage() {
   return <ErrorScreen title="404" description="페이지를 찾을 수 없습니다." />;
