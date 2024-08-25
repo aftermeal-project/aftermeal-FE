718edc240a6570f91d9e28d779133716f4799962
jongjinnn <jongjin.me@gmail.com>
2024-08-26 00:22:00 +0900

    refactor: features 폴더 import & export 방식 개선

---
+++ src/features/activity/components/index.ts
@@ -0,0 +1,3 @@
+export { default as ActivityListContainer } from './ActivityListContainer';
+export { default as ActivityListFallback } from './ActivityListFallback';
+export { default as ActivityListFetcher } from './ActivityListFetcher';
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -5,10 +5,12 @@ import {
   HomePageContainer,
 } from '../../components/ui/home';
 import { errorMessages } from '../../constants';
-import ActivityListContainer from '../../features/activity/components/ActivityListContainer';
-import ActivityListFetcher from '../../features/activity/components/ActivityListFetcher';
 import { Activity } from '../../types/activities';
-import ActivityListFallback from '../../features/activity/components/ActivityListFallback';
+import {
+  ActivityListFallback,
+  ActivityListFetcher,
+  ActivityListContainer,
+} from '../../features/activity';
 
 export default function HomePage() {
   return (
