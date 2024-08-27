57ae9b96c1fa3e1e9ad6228fd7d0094f9e076461
jongjinnn <jongjin.me@gmail.com>
2024-08-27 13:57:06 +0900

    refactor: LoginPage에서 LoginForm 분리

---
+++ src/features/auth/api/login.ts
@@ -0,0 +1,67 @@
+import { useMutation } from '@tanstack/react-query';
+import { LoginAPI } from '../../../libs/api/auth';
+import Token from '../../../libs/utils/token';
+import { LoginRequest } from '../../../types/auth';
+import { AxiosError } from 'axios';
+import { errorMessages, validationMessages } from '../../../constants';
+import { FieldError, UseFormSetError } from 'react-hook-form';
+import { NavigateFunction } from 'react-router-dom';
+
+const token = new Token();
+
+interface useLoginProps {
+  setError: UseFormSetError<LoginRequest>;
+  navigate: NavigateFunction;
+}
+
+interface handleLoginErrorProps {
+  error: any;
+  setError: UseFormSetError<LoginRequest>;
+}
+
+function handleLoginError({ error, setError }: handleLoginErrorProps) {
+  if (error instanceof AxiosError) {
+    const { response } = error;
+
+    const errorMapping: Record<number, FieldError> = {
+      400: {
+        type: 'manual',
+        message: validationMessages.WRONG_PASSWORD,
+      },
+      404: {
+        type: 'manual',
+        message: validationMessages.INVALID_CREDENTIALS,
+      },
+    };
+
+    const fieldError = errorMapping[Number(response?.status)] || {
+      type: 'manual',
+      message: errorMessages.UNKNOWN_ERROR,
+    };
+
+    setError(response?.status === 400 ? 'password' : 'email', fieldError);
+  } else {
+    setError('email', {
+      type: 'Unknown Error',
+      message: errorMessages.UNKNOWN_ERROR,
+    });
+  }
+}
+
+export default function useLogin({ setError, navigate }: useLoginProps) {
+  const mutation = useMutation({
+    mutationFn: (data: LoginRequest) => LoginAPI(data),
+    onSuccess: data => {
+      token.setUser(data);
+      navigate('/');
+    },
+    onError: (error: any) => {
+      handleLoginError({ error, setError });
+    },
+  });
+
+  return {
+    login: mutation.mutate,
+    isLoading: mutation.isPending,
+  };
+}
--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,100 +1,10 @@
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { LoginRequest } from '../../types/auth';
-import { AxiosError } from 'axios';
-import { LoginAPI } from '../../libs/api/auth';
-import { Link, useNavigate } from 'react-router-dom';
-import Token from '../../libs/utils/token';
-import { loginValidationRules, validationMessages } from '../../constants';
-import {
-  AuthFormContainer,
-  AuthInput,
-  AuthErrorMessages,
-  AuthButton,
-  AuthLoadingSpinner,
-} from '../../components/ui/auth';
+import { AuthFormContainer } from '../../components/ui/auth';
+import LoginForm from '../../features/auth/components/LoginForm';
 
 export default function LoginPage() {
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState<boolean>(false);
-  const token = new Token();
-
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors },
-  } = useForm<LoginRequest>();
-
-  function handleLoginError(error: any) {
-    if (error instanceof AxiosError) {
-      const { response } = error;
-
-      switch (response?.status) {
-        case 400:
-          setError('password', {
-            type: 'Password Error',
-            message: validationMessages.WRONG_PASSWORD,
-          });
-          break;
-        case 404:
-          setError('email', {
-            type: 'Credentials Error',
-            message: validationMessages.INVALID_CREDENTIALS,
-          });
-          break;
-      }
-    }
-  }
-
-  async function onValid(data: LoginRequest) {
-    setLoading(true);
-    try {
-      const response = await LoginAPI(data);
-      token.setUser(response);
-      navigate('/');
-    } catch (error: unknown) {
-      handleLoginError(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
   return (
     <AuthFormContainer title="애프터밀">
-      <form id="loginForm" onSubmit={handleSubmit(onValid)}>
-        <AuthInput<LoginRequest>
-          label="이메일"
-          name="email"
-          type="email"
-          placeholder="이메일"
-          register={register}
-          validationRules={loginValidationRules.emailValidationRules}
-          margin="mb-4"
-          srOnlyClass="sr-only"
-        />
-        <AuthInput<LoginRequest>
-          label="비밀번호"
-          name="password"
-          type="password"
-          placeholder="비밀번호"
-          register={register}
-          validationRules={loginValidationRules.passwordValidationRules}
-          margin={`${errors.email || errors.password ? '' : 'mb-7'}`}
-          srOnlyClass="sr-only"
-        />
-        <AuthErrorMessages errors={errors} fields={['email', 'password']} />
-        <AuthButton text={'로그인'} type="submit" disabled={loading} />
-        <div className="mt-5">
-          <Link
-            to="/signup"
-            className="text-sm text-[#0b57d0] transition duration-300 hover:underline"
-          >
-            계정 만들기
-          </Link>
-        </div>
-      </form>
-      <AuthLoadingSpinner loading={loading} text={'로그인 중'} />
+      <LoginForm />
     </AuthFormContainer>
   );
 }
