16216350f42e89d14563e5d197549cb40804eaeb
jongjinnn <jongjin.me@gmail.com>
2024-08-29 17:24:23 +0900

    refactor: types 폴더 구조 개선

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export * from './activities/activity-creatation-request-dto';
+export * from './activities/activity-list-response-dto';
+export * from './activities/activity-update-request-dto';
+
+export * from './activity-schedules/activity-schedule-list-response-dto';
+export * from './activity-schedules/activity-schedule-response-dto';
+export * from './activity-schedules/activity-schedule-response-dto-participants-inner';
+export * from './activity-schedules/activity-schedule-status-update-dto';
+
+export * from './authentications/login-request-dto';
+export * from './authentications/login-response-dto';
+export * from './authentications/login-response-dto-user';
+export * from './authentications/token-refresh-request-dto';
+export * from './authentications/token-refresh-response-dto';
+
+export * from './participations/participation-application-request-dto';
+export * from './participations/participation-cancellation-request-dto';
+export * from './participations/participation-controller-apply-participation201-response';
+
+export * from './users/user-list-response-dto';
+export * from './users/user-registration-request-dto';
+export * from './users/user-update-request-dto';
