c5969c41d567c6c4e3125d4d19ec61fe4efb283b
jongjinnn <jongjin.me@gmail.com>
2024-08-30 18:49:49 +0900

    refactor: import 경로 수정

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,8 +1,8 @@
 import { setupWorker } from 'msw/browser';
 import { authHandlers } from './auth.handler';
 import { userHandlers } from './user.handler';
-import { activitiesHandlers } from './activities';
-import { activitySchedulesHandlers } from './activity-schedules';
+import { activitiesHandlers } from './activities.handlers';
+import { activitySchedulesHandlers } from './activity-schedules.handlers';
 
 export const worker = setupWorker(
   ...authHandlers,
