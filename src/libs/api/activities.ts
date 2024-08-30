a64dc652c09872db612fba1588be4c9eaee3b668
jongjinnn <jongjin.me@gmail.com>
2024-08-30 16:22:37 +0900

    add: activities api

--- src/libs/api/activities.ts
+++ src/libs/api/activities.ts
@@ -1,10 +1,50 @@
-import { GetActivitiesResponse } from '../../types/activities';
+import {
+  ActivityCreatationRequestDto,
+  ActivityListResponseDto,
+  ActivityUpdateRequestDto,
+} from '../../types';
 import { instance } from '../instance';
 
+const url = '/activities';
+
 export const GetActivitiesAPI = async () => {
-  const response = await instance<GetActivitiesResponse>({
+  const response = await instance<ActivityListResponseDto>({
     method: 'GET',
-    url: '/v1/activities',
+    url: url,
+  });
+
+  return response.data;
+};
+
+export const CreateActivityAPI = async (
+  createActivityData: ActivityCreatationRequestDto,
+) => {
+  const response = await instance({
+    method: 'POST',
+    url: url,
+    data: createActivityData,
+  });
+
+  return response.data;
+};
+
+export const EditActivityAPI = async (
+  activityUpdateData: ActivityUpdateRequestDto,
+) => {
+  const response = await instance({
+    method: 'PUT',
+    url: url,
+    data: activityUpdateData,
+  });
+
+  return response.data;
+};
+
+export const DeleteActivityAPI = async (activityId: string) => {
+  const response = await instance({
+    method: 'DELETE',
+    url: url,
+    params: activityId,
   });
 
   return response.data;
