aef1586eebd72de908c4d53da143deb617ca71de
jongjinnn <jongjin.me@gmail.com>
2024-08-30 16:22:09 +0900

    add: activity-schedules mocking api

---
+++ src/mocks/activity-schedules.ts
@@ -0,0 +1,45 @@
+import { http, HttpResponse } from 'msw';
+import { BASE_URL } from '../constants';
+import {
+  ActivityScheduleListResponseDto,
+  ActivityScheduleListResponseDtoType,
+  ActivityScheduleStatusUpdateDtoStatus,
+} from '../types';
+import { faker } from '@faker-js/faker';
+
+function getRandomtype() {
+  const statuses = Object.values(ActivityScheduleListResponseDtoType);
+  const randomIndex = faker.number.int({ min: 0, max: statuses.length - 1 });
+  return statuses[randomIndex];
+}
+
+function getRandomStatus() {
+  const statuses = Object.values(ActivityScheduleStatusUpdateDtoStatus);
+  const randomIndex = faker.number.int({ min: 0, max: statuses.length - 1 });
+  return statuses[randomIndex];
+}
+
+function createRandomActivitySchedules(): ActivityScheduleListResponseDto {
+  return {
+    activityScheduleId: faker.number.int({ min: 1, max: 10 }),
+    name: faker.person.fullName(),
+    currentParticipants: faker.number.int({ min: 0, max: 1 }),
+    maxParticipants: faker.number.int({ min: 2, max: 20 }),
+    status: getRandomStatus(),
+    type: getRandomtype(),
+    scheduledDate: String(faker.date.future()),
+  };
+}
+
+export const activitySchedulesHandlers = [
+  // 활동 일정 리스트 조회
+  http.get(BASE_URL + '/activity-schedules', async () => {
+    const activityScheduleList = Array.from({ length: 5 }, () =>
+      createRandomActivitySchedules(),
+    );
+
+    return HttpResponse.json({
+      data: activityScheduleList,
+    });
+  }),
+];
