d89874cbd45ac25207c07534520233a9153ad7cf
jongjinnn <jongjin.me@gmail.com>
2024-08-31 01:00:24 +0900

    refactor: 컴포넌트 구조 개선

--- src/components/ui/admin/tab/ActivityManagementTab.tsx
+++ src/components/ui/admin/tab/ActivityManagementTab.tsx
@@ -1,10 +1,5 @@
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { ModalAtomFamily } from '../../../../atoms';
-import ActivityManagementTable from '../table/ActivityManagementTable';
-import ConfirmDeleteModal from '../../modal/ConfirmDeleteModal';
-import CreateActivityModal from '../../modal/CreateActivityModal';
-import { AtomKeys } from '../../../../constants';
 import { ActivityListResponseDto } from '../../../../types';
+import { TableHeader, TableBody } from '../table';
 
 interface ActivityManagementTabProps {
   activities: ActivityListResponseDto[];
@@ -13,33 +8,10 @@ interface ActivityManagementTabProps {
 export default function ActivityManagementTab({
   activities,
 }: ActivityManagementTabProps) {
-  const deleteModal = useRecoilValue(ModalAtomFamily(AtomKeys.DELETE_ACTIVITY));
-  const [createModal, setCreateModal] = useRecoilState(
-    ModalAtomFamily(AtomKeys.CREATE_ACTIVITY),
-  );
-
-  function onCreateActivity() {
-    setCreateModal(true);
-  }
-
   return (
-    <>
-      {deleteModal && <ConfirmDeleteModal />}
-      {createModal && <CreateActivityModal />}
-      <div className="h-full overflow-hidden">
-        <div className="flex items-center justify-between mb-4">
-          <h1 className="text-2xl font-bold">활동 관리</h1>
-          <button
-            className="rounded-md bg-green-500 px-3 py-[0.4rem] text-white"
-            onClick={onCreateActivity}
-          >
-            활동 추가
-          </button>
-        </div>
-        <div className="overflow-x-auto">
-          <ActivityManagementTable activities={activities} />
-        </div>
-      </div>
-    </>
+    <table className="w-full min-w-[950px] table-auto border-collapse border border-gray-200 shadow-md">
+      <TableHeader />
+      <TableBody activities={activities} />
+    </table>
   );
 }
--- src/components/ui/admin/table/index.ts
+++ src/components/ui/admin/table/index.ts
@@ -1,1 +1,2 @@
-export { default as ActivityManagementTable } from './ActivityManagementTable';
+export { default as TableHeader } from './TableHeader';
+export { default as TableBody } from './TableBody';
