3d290d935c5f1531db4c01ddafcdda62d4dc3b73
jongjinnn <jongjin.me@gmail.com>
2024-08-30 16:23:16 +0900

    add: activitiesHandlers, activitySchedulesHandlers in setUpWorker

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,5 +1,12 @@
 import { setupWorker } from 'msw/browser';
 import { authHandlers } from './auth.handler';
 import { userHandlers } from './user.handler';
+import { activitiesHandlers } from './activities';
+import { activitySchedulesHandlers } from './activity-schedules';
 
-export const worker = setupWorker(...authHandlers, ...userHandlers);
+export const worker = setupWorker(
+  ...authHandlers,
+  ...userHandlers,
+  ...activitiesHandlers,
+  ...activitySchedulesHandlers,
+);
