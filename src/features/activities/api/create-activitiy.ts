be2316bfed330782cc235cbdd33aa7bee082bd37
jongjinnn <jongjin.me@gmail.com>
2024-09-02 00:30:00 +0900

    refactor: 활동 생성 기능 구현

---
+++ src/features/activities/api/create-activitiy.ts
@@ -0,0 +1,46 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  ActivityCreatationRequestDto,
+  ActivityListResponseDto,
+} from '../../../types';
+import { CreateActivityAPI } from '../../../libs/api/activities';
+import { errorMessages } from '../../../constants';
+
+async function createActivity(data: ActivityCreatationRequestDto) {
+  return await CreateActivityAPI(data);
+}
+
+function handleCreateActivityError() {
+  alert(errorMessages.UNKNOWN_ERROR);
+}
+
+export default function useCreateActivity() {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: createActivity,
+    onMutate: async (newActivity: ActivityCreatationRequestDto) => {
+      await queryClient.cancelQueries({ queryKey: ['activities'] });
+
+      const previousActivities = queryClient.getQueryData<
+        ActivityListResponseDto[]
+      >(['activities']);
+
+      queryClient.setQueryData(
+        ['activities'],
+        (old: ActivityListResponseDto[]) => [...old, newActivity],
+      );
+
+      return { previousActivities };
+    },
+    onError: (_error, _variables, context: any) => {
+      queryClient.setQueryData(['activities'], context.previousActivities);
+      handleCreateActivityError();
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['activities'] });
+    },
+  });
+
+  return { createActivity: mutation };
+}
---
+++ src/features/activities/components/CreateActivityModal.tsx
@@ -0,0 +1,93 @@
+import { useSetRecoilState } from 'recoil';
+import ModalLayout from '../../../components/@global/layout/ModalLayout';
+import { useForm } from 'react-hook-form';
+import { ModalAtomFamily } from '../../../atoms';
+import { AtomKeys } from '../../../constants';
+import { Input } from '../../../components/@global';
+import { ActivityCreatationRequestDto } from '../../../types';
+import useCreateActivity from '../api/create-activitiy';
+
+export default function CreateActivityModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ActivityCreatationRequestDto>({
+    defaultValues: {
+      name: '',
+      location: '',
+      maxParticipants: 2,
+    },
+  });
+
+  const { createActivity } = useCreateActivity();
+
+  const setModal = useSetRecoilState(ModalAtomFamily(AtomKeys.CREATE_ACTIVITY));
+
+  function onSubmit(data: ActivityCreatationRequestDto) {
+    // check validation
+
+    createActivity.mutate(data, {
+      onSuccess: () => setModal(false),
+    });
+  }
+
+  function handleModalClose() {
+    setModal(false);
+  }
+
+  return (
+    <ModalLayout setModal={setModal}>
+      <div
+        className="p-6 mx-auto bg-white rounded-lg shadow-lg w-80"
+        onClick={e => e.stopPropagation()}
+      >
+        <h2 className="mb-8 text-lg font-bold">활동 추가</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input<ActivityCreatationRequestDto>
+            label="이름"
+            name="name"
+            type="text"
+            placeholder="이름"
+            register={register}
+            margin="mb-4"
+            error={errors.name}
+          />
+          <Input<ActivityCreatationRequestDto>
+            label="장소"
+            name="location"
+            type="text"
+            placeholder="장소"
+            register={register}
+            margin="mb-4"
+            error={errors.location}
+          />
+          <Input<ActivityCreatationRequestDto>
+            label="최대 참가자 수"
+            name="maxParticipants"
+            type="number"
+            placeholder="최대 참가자 수"
+            register={register}
+            margin="mb-4"
+            error={errors.maxParticipants}
+          />
+          <div className="flex justify-between w-full mt-11">
+            <button
+              type="button"
+              onClick={handleModalClose}
+              className="px-4 py-2 text-gray-700 bg-gray-300 rounded-md hover:bg-gray-400"
+            >
+              취소
+            </button>
+            <button
+              type="submit"
+              className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+            >
+              추가
+            </button>
+          </div>
+        </form>
+      </div>
+    </ModalLayout>
+  );
+}
