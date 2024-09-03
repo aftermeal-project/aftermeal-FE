a0a2bd35b4dbaa295cc60d673cdc7c288d6c7e5f
jongjinnn <jongjin.me@gmail.com>
2024-09-03 17:45:29 +0900

    refactor: bg color 수정

--- src/components/ui/admin/container/AdminPageContainer.tsx
+++ src/components/ui/admin/container/AdminPageContainer.tsx
@@ -8,6 +8,6 @@ export default function AdminPageContainer({
   children,
 }: AdminPageContainerProps) {
   return (
-    <main className="flex-1 overflow-hidden bg-gray-100 p-6">{children}</main>
+    <main className="flex-1 overflow-hidden bg-white p-6">{children}</main>
   );
 }
