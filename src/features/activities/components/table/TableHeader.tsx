c0637e0f4ad92753a4304099ac27de9700f3319b
jongjinnn <jongjin.me@gmail.com>
2024-09-03 13:21:38 +0900

    refactor: table ui 개선

--- src/features/activities/components/table/TableHeader.tsx
+++ src/features/activities/components/table/TableHeader.tsx
@@ -13,10 +13,11 @@ import HeaderCell from '../cell/HeaderCell';
 export default function TableHeader() {
   return (
     <thead>
-      <tr className="bg-gray-100">
+      <tr className="w-fit bg-gray-100">
         <HeaderCell icon={FaUser} text="이름" />
         <HeaderCell icon={FaMapMarkerAlt} text="장소" />
         <HeaderCell icon={FaUsers} text="참여 현황" />
+        <HeaderCell icon={FaUsers} text="최대 참가자" />
         <HeaderCell icon={FaCheckCircle} text="진행 상태" />
         <HeaderCell icon={FaCalendar} text="세션 유형" />
         <HeaderCell icon={FaCalendarDay} text="일정 날짜" />
