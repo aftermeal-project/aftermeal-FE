3afc53d861838793e5e4bf2158b97cea1e988274
jongjinnn <jongjin.me@gmail.com>
2024-09-03 17:46:48 +0900

    refactor: 항목명 수정

--- src/features/activities/components/table/TableHeader.tsx
+++ src/features/activities/components/table/TableHeader.tsx
@@ -6,7 +6,7 @@ import {
   FaCalendarDay,
   FaCheckCircle,
   FaClock,
-  FaUser,
+  FaSocks,
 } from 'react-icons/fa';
 import HeaderCell from '../cell/HeaderCell';
 
@@ -14,15 +14,15 @@ export default function TableHeader() {
   return (
     <thead>
       <tr className="w-fit bg-gray-100">
-        <HeaderCell icon={FaUser} text="이름" />
+        <HeaderCell icon={FaSocks} text="활동명" />
         <HeaderCell icon={FaMapMarkerAlt} text="장소" />
         <HeaderCell icon={FaUsers} text="참여 현황" />
         <HeaderCell icon={FaUsers} text="최대 참가자" />
         <HeaderCell icon={FaCheckCircle} text="진행 상태" />
         <HeaderCell icon={FaCalendar} text="세션 유형" />
-        <HeaderCell icon={FaCalendarDay} text="일정 날짜" />
-        <HeaderCell icon={FaClock} text="신청시작 시간" />
-        <HeaderCell icon={FaClock} text="신청종료 시간" />
+        <HeaderCell icon={FaCalendarDay} text="일정" />
+        <HeaderCell icon={FaClock} text="신청 시작" />
+        <HeaderCell icon={FaClock} text="신청 마감" />
         <HeaderCell icon={FaCog} text="관리" />
       </tr>
     </thead>
