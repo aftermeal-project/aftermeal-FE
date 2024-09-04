a3a34c31adb53035e769054382f546ed2f5e872a
jongjinnn <jongjin.me@gmail.com>
2024-09-04 15:29:16 +0900

    refactor: TableBodyCell 코드 개선

--- src/features/activities/components/cell/BodyCell.tsx
+++ src/features/activities/components/cell/BodyCell.tsx
@@ -1,6 +1,6 @@
 import React, { InputHTMLAttributes } from 'react';
 import { Path, UseFormRegister, UseFormSetValue } from 'react-hook-form';
-import { ActivityResponseDto } from '../../../../types';
+import { ActivityResponseDto, Option } from '../../../../types';
 import SelectField from '../select/SelectField';
 import { formatTime } from '../../../../utils';
 
@@ -9,7 +9,7 @@ interface BodyCellProps extends InputHTMLAttributes<HTMLInputElement> {
   isEditing: boolean;
   value: string | number;
   register: UseFormRegister<ActivityResponseDto>;
-  options?: { value: string; label: string }[];
+  options?: Option[];
   setValue?: UseFormSetValue<ActivityResponseDto>;
 }
 
@@ -20,44 +20,60 @@ export default function BodyCell({
   register,
   options,
   setValue,
+  type,
   ...rest
 }: BodyCellProps) {
-  const { type } = rest;
-
-  if (setValue && isEditing) {
+  /**
+   * Cell이 edit mode 일 때 time 포맷팅
+   */
+  if (setValue && isEditing && type === 'time') {
     setValue(title, formatTime({ type: 'format', time: value as string }));
   }
 
+  /**
+   * read-only cell 렌더링
+   */
   if (!isEditing) {
-    const time = formatTime({ type: 'readable', time: value as string });
+    const displayValue =
+      type === 'time'
+        ? formatTime({ type: 'readable', time: value as string })
+        : value;
 
     return (
       <td className="border border-gray-200 px-2">
         <span className="block w-full px-2 py-1 text-gray-800">
-          {type === 'time' ? time : value}
+          {displayValue}
         </span>
       </td>
     );
   }
 
-  if (type === 'select') {
+  /**
+   * 'select' 타입일 때  SelectField 렌더링
+   */
+  if (type === 'select' && options) {
     return (
       <td className="border border-gray-200 px-2">
         <SelectField<ActivityResponseDto>
           title={title}
           value={value}
           register={register}
-          options={options as { value: string; label: string }[]}
+          options={options}
         />
       </td>
     );
   }
 
+  /**
+   * 기타 타입일 때 input field 렌더링
+   */
   return (
     <td className="border border-gray-200 px-2">
       <input
         type={type}
-        className={`box-border w-full rounded-md border border-gray-300 px-2 py-1 ${title === 'maxParticipants' && 'w-24'}`}
+        className={`box-border w-full rounded-md border border-gray-300 px-2 py-1 ${
+          title === 'maxParticipants' && 'w-24'
+        }`}
         placeholder={value as string}
         {...register(title, { required: true })}
         {...rest}
