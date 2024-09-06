f90c599b784fb42a1a15fc82404850e275358f70
jongjinnn <jongjin.me@gmail.com>
2024-09-06 16:05:30 +0900

    refactor: UserList skeleton UI 개선

--- src/features/users/components/skeleton/UserListSkeleton.tsx
+++ src/features/users/components/skeleton/UserListSkeleton.tsx
@@ -1,15 +1,19 @@
+import Skeleton from 'react-loading-skeleton';
 import SkeletonUser from './SkeletonUser';
 
 export default function UserListSkeleton() {
   return (
-    <div className="w-full px-6 py-3 bg-white border border-gray-200 rounded-lg shadow">
-      <div className="flow-root">
-        <ul className="divide-y divide-gray-200">
-          {Array.from({ length: 8 }).map(() => (
-            <SkeletonUser key={crypto.randomUUID()} />
-          ))}
-        </ul>
+    <section>
+      <Skeleton width="full" height={42} className="mb-4" />
+      <div className="w-full px-6 py-3 bg-white border border-gray-200 rounded-lg shadow">
+        <div className="flow-root">
+          <ul className="divide-y divide-gray-200">
+            {Array.from({ length: 8 }).map(() => (
+              <SkeletonUser key={crypto.randomUUID()} />
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
