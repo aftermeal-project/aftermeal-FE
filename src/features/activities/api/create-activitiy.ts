1072451f1249ef9b764d8fbe2e1cf039c0a46eef
jongjinnn <jongjin.me@gmail.com>
2024-09-07 15:09:53 +0900

    refactor: useCreateActivity에 optimistic update 적용

--- src/features/activities/api/create-activitiy.ts
+++ src/features/activities/api/create-activitiy.ts
@@ -1,5 +1,8 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { ActivityCreationRequestDto } from '../../../types';
+import {
+  ActivityCreationRequestDto,
+  ActivityResponseDto,
+} from '../../../types';
 import { errorMessages } from '../../../constants';
 import { CreateActivityAPI } from '../../../libs/api/admin.activities';
 
@@ -15,6 +18,18 @@ export default function useCreateActivity() {
 
   const mutation = useMutation<void, Error, ActivityCreationRequestDto>({
     mutationFn: createActivity,
+    onMutate: async newActivity => {
+      await queryClient.cancelQueries({ queryKey: ['activities'] });
+
+      const previousActivities = queryClient.getQueryData(['activities']);
+
+      queryClient.setQueryData(['activities'], (old: ActivityResponseDto[]) => [
+        ...old,
+        newActivity,
+      ]);
+
+      return { previousActivities };
+    },
     onError: (_error, _variables, context: any) => {
       queryClient.setQueryData(['activities'], context.previousActivities);
       handleCreateActivityError();
