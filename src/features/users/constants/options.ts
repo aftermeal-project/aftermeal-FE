1636a29643ef1799172c3477db957d7587b64643
jongjinnn <jongjin.me@gmail.com>
2024-09-07 13:41:38 +0900

    add: User selector options

---
+++ src/features/users/constants/options.ts
@@ -0,0 +1,9 @@
+export const typeOptions = [
+  { value: 'STUDENT', label: 'STUDENT' },
+  { value: 'TEACHER', label: 'TEACHER' },
+];
+
+export const statusOptions = [
+  { value: 'ACTIVATE', label: 'ACTIVATE' },
+  { value: 'DEACTIVATE', label: 'DEACTIVATE' },
+];
