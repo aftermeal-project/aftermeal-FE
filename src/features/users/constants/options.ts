659425848a13fb5ae6a16aa5c12ed84ebd39f9aa
jongjinnn <jongjin.me@gmail.com>
2024-09-08 15:30:32 +0900

    refactor: options label 한국어로 변경

--- src/features/users/constants/options.ts
+++ src/features/users/constants/options.ts
@@ -1,9 +1,9 @@
 export const typeOptions = [
-  { value: 'STUDENT', label: 'STUDENT' },
-  { value: 'TEACHER', label: 'TEACHER' },
+  { value: 'STUDENT', label: '학생' },
+  { value: 'TEACHER', label: '선생님' },
 ];
 
 export const statusOptions = [
-  { value: 'ACTIVATE', label: 'ACTIVATE' },
-  { value: 'DEACTIVATE', label: 'DEACTIVATE' },
+  { value: 'ACTIVATE', label: '활성화' },
+  { value: 'DEACTIVATE', label: '비활성화' },
 ];
