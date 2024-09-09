89c3afe5a26df07a08078eb35dce35f236dab28a
jongjinnn <jongjin.me@gmail.com>
2024-09-09 12:34:47 +0900

    add: 활동 상세 조회 페이지 추가

--- src/routes/routing.tsx
+++ src/routes/routing.tsx
@@ -5,11 +5,16 @@ import LoginPage from '../pages/auth/LoginPage';
 import SignupPage from '../pages/auth/SignupPage';
 import NotFoundPage from '../pages/not-found/NotFoundPage';
 import AdminPage from '../pages/admin/AdminPage';
+import ActivityDetailsPage from '../pages/activity-details/ActivityDetailsPage';
 
 export default function Routing() {
   return (
     <Routes>
       <Route path={Path.HomePage} element={<HomePage />} />
+      <Route
+        path={Path.ActivityDetailsPage}
+        element={<ActivityDetailsPage />}
+      />
       <Route path={Path.LoginPage} element={<LoginPage />} />
       <Route path={Path.SignupPage} element={<SignupPage />} />
       <Route path={Path.AdminPage} element={<AdminPage />} />
