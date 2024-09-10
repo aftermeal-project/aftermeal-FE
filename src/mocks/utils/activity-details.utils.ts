3a2ddcad9673927389840457fae47f761d700fa5
jongjinnn <jongjin.me@gmail.com>
2024-09-10 20:55:48 +0900

    refactor: dummy data를 실제 데이터처럼 개선

--- src/mocks/utils/activity-details.utils.ts
+++ src/mocks/utils/activity-details.utils.ts
@@ -4,33 +4,68 @@ import {
   ActivityResponseDtoStatus,
   ActivityDetailResponseDto,
 } from '../../types';
-import moment from 'moment';
 import { getRandomElementFromArray } from './global.utils';
+
+const sports = ['축구', '농구', '배드민턴', '배구', '골프', '탁구'];
+
+const getRandomSport = () => {
+  return faker.helpers.arrayElement(sports);
+};
 
 function getParticipants() {
   const participants = {
     id: faker.number.int({ min: 1, max: 1000000 }),
-    name: faker.person.fullName(),
+    displayName: faker.person.firstName(),
   };
 
   return participants;
 }
 
-function formatDate(date: string) {
-  return moment(date).format('YYYY-MM-DD');
+function getStartAndEndTimes(type: ActivityResponseDtoType) {
+  if (type === 'LUNCH') {
+    return {
+      startTime: '11:30',
+      endTime: '12:20',
+    };
+  } else if (type === 'DINNER') {
+    return {
+      startTime: '17:30',
+      endTime: '18:20',
+    };
+  }
+  return {
+    startTime: '09:00',
+    endTime: '10:00',
+  };
 }
 
 export function createRandomActivityDetails(): ActivityDetailResponseDto {
+  const type = getRandomElementFromArray(
+    Object.values(ActivityResponseDtoType),
+  );
+  const { startTime, endTime } = getStartAndEndTimes(type);
+  const now = new Date();
+
+  // Set the time for applicationStartDate and applicationEndDate
+  const startDate = new Date(now);
+  const endDate = new Date(now);
+
+  const [startHour, startMinute] = startTime.split(':').map(Number);
+  const [endHour, endMinute] = endTime.split(':').map(Number);
+
+  startDate.setHours(startHour, startMinute, 0);
+  endDate.setHours(endHour, endMinute, 0);
+
   return {
     id: faker.number.int({ min: 1, max: 1000000 }),
-    title: faker.person.fullName(),
+    title: getRandomSport(),
     location: faker.location.city(),
     maxParticipants: faker.number.int({ min: 2, max: 20 }),
     status: getRandomElementFromArray(Object.values(ActivityResponseDtoStatus)),
-    type: getRandomElementFromArray(Object.values(ActivityResponseDtoType)),
-    scheduledDate: formatDate(moment(faker.date.soon()).toISOString()),
-    applicationStartDate: String(faker.date.soon()),
-    applicationEndDate: String(faker.date.soon()),
+    type: type,
+    scheduledDate: String(now),
+    applicationStartDate: String(startDate),
+    applicationEndDate: String(endDate),
     participants: Array.from({ length: 5 }, () => getParticipants()),
   };
 }
