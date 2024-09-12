2d03441a0d428dececaf4abfb99701de82642320
jongjinnn <jongjin.me@gmail.com>
2024-09-12 14:42:08 +0900

    refactor: 사용하지 않는 컴포넌트 제거

--- src/features/activities/components/skeleton/index.ts
+++ src/features/activities/components/skeleton/index.ts
@@ -1,4 +1,3 @@
 export { default as ActivityListSkeleton } from './ActivityListSkeleton';
-export { default as SkeletonCard } from './SkeletonCard';
 export { default as SkeletonList } from './SkeletonList';
 export { default as SkeletonTable } from './SkeletonTable';
