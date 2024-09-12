c57e40e8d7427d2e4dde9ca5058e1fe77ca6b8e5
jongjinnn <jongjin.me@gmail.com>
2024-09-12 14:41:03 +0900

    refactor: header width 조정에 따른 HomePageContainer width 조정

--- src/components/container/HomePageContainer.tsx
+++ src/components/container/HomePageContainer.tsx
@@ -8,10 +8,8 @@ export default function HomePageContainer({
   children,
 }: HomePageContainerProps) {
   return (
-    <main className="w-full px-4 py-8">
-      <div className="mx-auto grid max-w-screen-xl grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {children}
-      </div>
+    <main className="relative flex items-center w-full px-4">
+      <div className="mx-auto w-full max-w-[1000px]">{children}</div>
     </main>
   );
 }
