570252fd161df29d209d7f9cbc18f49e2d2caa46
jongjinnn <jongjin.me@gmail.com>
2024-09-18 17:46:23 +0900

    refactor: 사용하지 않는 컴포넌트 제거

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,7 +8,6 @@ export * from './layout';
 export * from './input';
 export * from './loading';
 export * from './modal';
-export * from './screen';
 export * from './selector';
 export * from './sidebar';
 export * from './tab';
