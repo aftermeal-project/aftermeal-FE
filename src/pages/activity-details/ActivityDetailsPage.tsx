e17f41b8a13b0b2e7925c8576421acad6f30f249
jongjinnn <jongjin.me@gmail.com>
2024-09-23 05:27:56 +0900

    add: 어드민용 활동 상세 조회 페이지 추가

--- src/pages/activity-details/ActivityDetailsPage.tsx
+++ src/pages/activity-details/ActivityDetailsPage.tsx
@@ -4,23 +4,37 @@ import {
   ActivityDetailsFetcher,
   ActivityDetails,
   ActivityDetailsSkeleton,
+  AdminActivityDetails,
 } from '../../features/activity-details/component';
 import { useLocation } from 'react-router-dom';
 
 export default function ActivityDetailsPage() {
   const location = useLocation();
+  const isAdminPage = location.pathname.includes('/admin');
 
   return (
     <>
       <SEOHelmet
-        title="참가 신청"
-        description="지금 바로 활동 참가 신청을 완료하세요!"
+        title={isAdminPage ? '활동 상세 페이지' : '참가 신청'}
+        description={
+          isAdminPage
+            ? '관리자용 활동 상세 페이지입니다.'
+            : '지금 바로 활동 참가 신청을 완료하세요!'
+        }
         url={location.pathname}
       />
       <ErrorBoundary Fallback={ErrorFallback}>
         <Suspense fallback={<ActivityDetailsSkeleton />}>
           <ActivityDetailsFetcher>
-            {activityDetails => <ActivityDetails activity={activityDetails} />}
+            {activityDetails => (
+              <>
+                {isAdminPage ? (
+                  <AdminActivityDetails activity={activityDetails} />
+                ) : (
+                  <ActivityDetails activity={activityDetails} />
+                )}
+              </>
+            )}
           </ActivityDetailsFetcher>
         </Suspense>
       </ErrorBoundary>
