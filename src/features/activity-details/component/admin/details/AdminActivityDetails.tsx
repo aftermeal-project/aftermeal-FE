37850ea5478bd10e22adc1d8a96ba3e93c8f45c8
jongjinnn <jongjin.me@gmail.com>
2024-09-23 08:23:08 +0900

    feat: 활동 삭제 기능 구현

--- src/features/activity-details/component/admin/details/AdminActivityDetails.tsx
+++ src/features/activity-details/component/admin/details/AdminActivityDetails.tsx
@@ -7,6 +7,12 @@ import { ButtonField } from '../button';
 import { UpdateActivityForm } from '../form';
 import { useForm } from 'react-hook-form';
 import { useUpdateActivityForm } from '../../../../../hooks/useUpdateActivityForm';
+import { useRecoilState, useResetRecoilState } from 'recoil';
+import { ActiveIdAtomFamily, ModalAtomFamily } from '../../../../../atoms';
+import { AtomKeys } from '../../../../../constants';
+import { ConfirmDeleteModal } from '../../../../../components';
+import useDeleteActivity from '../../../../activities/api/delete-activity';
+import { useNavigate } from 'react-router-dom';
 
 export type ModeType = 'Read' | 'Update';
 export type TabType = 'Basic' | 'Schedule' | 'User';
@@ -24,32 +30,58 @@ function getFormattedTitle(date: string, type: string, title: string) {
 export default function AdminActivityDetails({
   activity,
 }: AdminActivityDetailsProps) {
+  const navigate = useNavigate();
   const formMethods = useForm<ActivityDetailResponseDto>();
+  const { handleUpdate } = useUpdateActivityForm(formMethods);
 
   const [mode, setMode] = useState<ModeType>('Read');
   const [tab, setTab] = useState<TabType>('Basic');
 
-  const { handleUpdate } = useUpdateActivityForm(formMethods);
+  const [deleteModalOpen, setDeleteModalOpen] = useRecoilState(
+    ModalAtomFamily(AtomKeys.DELETE_ACTIVITY_MODAL),
+  );
+  const [activeId, setActiveId] = useRecoilState(
+    ActiveIdAtomFamily(AtomKeys.ACTIVE_ACTIVITY_ID),
+  );
+  const resetActivityId = useResetRecoilState(
+    ActiveIdAtomFamily(AtomKeys.ACTIVE_ACTIVITY_ID),
+  );
+
+  const { deleteActivity } = useDeleteActivity();
 
   const handleUpdateActivity = () => {
     handleUpdate(activity);
     setMode('Update');
   };
 
-  const handleCancelUpdateActivity = () => {
-    setMode('Read');
+  const handleDeleteActivity = (activityId: number) => {
+    setActiveId(activityId);
+    setDeleteModalOpen(true);
+  };
+
+  const handleSettled = () => {
+    resetActivityId();
+    navigate('/admin');
   };
 
   return (
     <main className="mx-auto min-h-screen w-full max-w-[1000px] rounded-lg bg-white p-6 shadow-lg">
-      <h2 className="text-3xl font-bold text-gray-800 mb-9">
+      {deleteModalOpen && (
+        <ConfirmDeleteModal
+          message="정말 해당 활동을 삭제하시겠습니까?"
+          modalKey={AtomKeys.DELETE_ACTIVITY_MODAL}
+          request={deleteActivity}
+          params={String(activeId)}
+          onSettled={handleSettled}
+        />
+      )}
+      <h2 className="mb-9 text-3xl font-bold text-gray-800">
         {getFormattedTitle(
           activity.scheduledDate,
           activity.type,
           activity.title,
         )}
       </h2>
-
       {mode === 'Read' ? (
         <ActivityDetailsTabsContainer activity={activity} setTab={setTab} />
       ) : (
@@ -60,14 +92,11 @@ export default function AdminActivityDetails({
           setMode={setMode}
         />
       )}
-
       {tab !== 'User' && mode === 'Read' && (
         <ButtonField
           mode={'Read'}
           onUpdate={handleUpdateActivity}
-          onDelete={() => console.log('')}
-          onSave={() => console.log('')}
-          onCancel={handleCancelUpdateActivity}
+          onDelete={() => handleDeleteActivity(activity.id)}
         />
       )}
     </main>
