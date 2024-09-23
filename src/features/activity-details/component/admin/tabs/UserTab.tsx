dce990f317aacaf76de4fd36e248510de6d961ad
jongjinnn <jongjin.me@gmail.com>
2024-09-23 15:44:50 +0900

    refactor: 어드민용 활동 상세 페이지 참가자 목록 배열 예외처리

--- src/features/activity-details/component/admin/tabs/UserTab.tsx
+++ src/features/activity-details/component/admin/tabs/UserTab.tsx
@@ -11,13 +11,17 @@ export default function ParticipantsTab({
   return (
     <div>
       <p className="mb-2 font-bold">참가자 목록</p>
-      {participations.map(item => (
-        <User
-          key={item.id}
-          user={item.user}
-          onDelete={() => console.log('d')}
-        />
-      ))}
+      {Array.isArray(participations) && participations.length > 0 ? (
+        participations.map(item => (
+          <User
+            key={item.id}
+            user={item.user}
+            onDelete={() => console.log('d')}
+          />
+        ))
+      ) : (
+        <p>아직 참가자가 없습니다</p>
+      )}
     </div>
   );
 }
